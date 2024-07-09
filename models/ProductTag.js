706b26840f25490570bc76db76de550588cce240
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-08 17:01:54 -0700

    added Id and model references to the productTag.js file

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,16 +8,25 @@ ProductTag.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     product_id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
     }
   },
   {
